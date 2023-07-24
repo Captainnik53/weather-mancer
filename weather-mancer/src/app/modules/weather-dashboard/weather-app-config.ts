9fd2483ac840388c59a3e148af1c47d6098e3863
Nikunj Gondha <nikunj.gondha@searce.com>
2023-07-24 16:59:20 +0530

    Added OpenAI chatGPT based weather bot

--- weather-mancer/src/app/modules/weather-dashboard/weather-app-config.ts
+++ weather-mancer/src/app/modules/weather-dashboard/weather-app-config.ts
@@ -1,4 +1,4 @@
 export const config = {
   apiKey: '9762e0fff8173369368a44617f7b8b45',
-  chatGPTApiKey: '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',
+  chatGPTApiKey: process.env['OPENAI_API_KEY'],
 };
