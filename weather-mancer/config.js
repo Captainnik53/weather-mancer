30d9d605b89433eef63b21e1c677d914d8b46179
Nikunj Gondha <nikunj.gondha@searce.com>
2023-07-28 17:57:59 +0530

    Added node script for api key

--- weather-mancer/config.js
+++ weather-mancer/config.js
@@ -5,7 +5,6 @@ const setEnv = () => {
     const targetPath = './src/environments/environment.ts';
   // Load node modules
     const colors = require('colors');
-    const appVersion = require('../../package.json').version;
     require('dotenv').config({
       path: 'src/environments/.env'
     });
